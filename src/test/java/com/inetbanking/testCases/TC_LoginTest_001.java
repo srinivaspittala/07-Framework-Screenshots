b46580e059202edd15587b6b1d9a933b1a65a27c
srinivasunites <srinivas.unites@gmail.com>
2018-12-30 20:46:26 +0530

    This is my first commit

---
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
@@ -0,0 +1,53 @@
+package com.inetbanking.testCases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.LoginPage;
+
+public class TC_LoginTest_001 extends BaseClass
+{
+	@Test
+	
+	public void LoginTest() throws IOException
+	{
+		driver.get(baseURL);
+		
+		logger.info("URL is Opened");
+		
+		LoginPage lp=new LoginPage(driver);
+		
+		           lp.setUserName(username);
+		           
+		           logger.info("Entered Username");
+		           		           
+		           lp.setPassword(password);
+		           
+		           logger.info("Entered Password");
+		         
+		           lp.clickSubmit();
+		           
+		           logger.info("Clicked Submit Button");
+		           
+		if(driver.getTitle().equals(exptitle))
+		{
+			Assert.assertTrue(true);
+			
+			logger.info("Login test is Passed");
+		}
+		else
+		{
+			captureScreen(driver,"loginTest");
+			
+			Assert.assertTrue(false);
+			
+			logger.info("Login test is Failed");
+		}
+					
+	}
+	 
+	 
+}
